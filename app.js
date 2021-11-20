f9fe4bfef50fc0bc4969d2bef477a04bc577d2e8
albertorodriguezdiaz <alberto@efectovirtual.com>
2021-11-19 23:55:47 -0500

    API Users

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('Back-end LearningPlatform');
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require('cors');
+
+// Crear el servidor
+const app = express();
+
+// Conectar a la base de datos
+conectarDB();
+
+// Habilitar cors
+app.use(cors());
+
+// habilitar express .json
+app.use(express.json({ extended: true}));
+
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Importar rutas
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+
+// Arrancar la app
+app.listen(PORT, () => {
+    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+});
---
+++ controllers/userController.js
@@ -0,0 +1,64 @@
+const Usuario = require('../models/User');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+
+exports.crearUsuario = async (req, res) => {
+
+    // revisar si hay errores
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({errores: errores.array()});
+    }
+
+    const {email, password} = req.body;
+
+    try {
+        // Revisar que el usuario registrado sea unico
+        let usuario = await Usuario.findOne({email});
+
+        if (usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe'});
+        }
+
+        // Crea el nuevo usuario
+        usuario = new Usuario(req.body);
+
+
+        // Hashear el password
+        // ( salt ) crea un hash diferente para passwords iguales
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
+
+
+        // Guardar usuario
+        await usuario.save();
+
+        // Crear y firmnar el JWT
+        const payload = {
+            usuario:{
+                id: usuario.id
+            }
+        };
+
+        // Firmar el JWT
+        jwt.sign(payload, process.env.SECRETA, {
+            expiresIn: 3600 // 1hora
+        }, (error, token) => {
+           
+            if(error) throw error;
+
+            // Mensaje de confirmacion
+            console.log(token);
+            res.json({ token: token, msg:'Usuario creado correctamente'});
+           
+        });
+
+        // Mensaje de confirmacion
+        // res.json({ msg: 'El usuario fue creado exitosamente'});
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }
+}
